045e83346b91fb47398c2fcf355584fa19d87083
madicke12 <cissemadicke8@gmail.com>
2024-01-08 23:32:48 +0000

    no commit message needed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth/next";
 import { PrismaClient } from "@prisma/client";
+import env from "@/app/env";
 
 const prisma = new PrismaClient();
 
@@ -10,20 +11,20 @@ export const authOption = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 
   callbacks: {
-    async redirect({ url, baseUrl }) {
+    async redirect({ url, baseUrl }:{url:String,baseUrl:String}) {
       // Allows relative callback URLs
       if (url.startsWith("/")) return `${baseUrl}${url}`;
       // Allows callback URLs on the same origin
       else if (new URL(url).origin === baseUrl) return url;
       return baseUrl;
     },
-    session: ({ session, token , user}) => ({
+    session: ({ session, token , user}:any) => ({
       ...session,
       user
     }),
